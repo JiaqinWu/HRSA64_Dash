ea253df7c55ce5254cd84c3bee6546090c4e14ba
Jiaqin Wu <jiaqinwu@Jiaqins-MacBook-Pro.local>
2025-05-22 13:42:44 -0400

    updated

--- HRSA64.py
+++ HRSA64.py
@@ -218,6 +218,7 @@ if "user_email" not in st.session_state:
 if st.session_state.role is None:
     #st.image("Georgetown_logo_blueRGB.png",width=200)
     #st.title("Welcome to the GU Technical Assistance Provider System")
+    st.set_page_config(page_title="GU TAP System")
     st.markdown(
         """
         <div style='
